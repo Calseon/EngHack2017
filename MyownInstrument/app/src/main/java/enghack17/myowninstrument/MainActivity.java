62aeaa4da62e2fb198c8b7a8e6390942b0088e67
Group project <null>
2023-07-29 15:23:39 -0700

    added accelerometer data readings

--- MyownInstrument/app/src/main/java/enghack17/myowninstrument/MainActivity.java
+++ MyownInstrument/app/src/main/java/enghack17/myowninstrument/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -16,19 +17,148 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.thalmic.myo.AbstractDeviceListener;
+import com.thalmic.myo.Arm;
 import com.thalmic.myo.DeviceListener;
 import com.thalmic.myo.Hub;
+import com.thalmic.myo.Myo;
+import com.thalmic.myo.Pose;
+import com.thalmic.myo.Quaternion;
+import com.thalmic.myo.Vector3;
+import com.thalmic.myo.XDirection;
 import com.thalmic.myo.scanner.ScanActivity;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
-    private static final int PERMISSION_REQUEST_FINE_LOCATION = 2;
     private static final String TAG = "MainActivity";
-    private DeviceListener mListener = new AbstractDeviceListener() {};
+
+    // Frontend UI
+    private TextView mTextView;
+    private TextView accelXData;
+    private TextView accelYData;
+    private TextView accelZData;
+    private LineGraphView graph;
+
     Hub hub;
+
+    // Device Listener for the Myo -----------------------------------------------------------
+    private DeviceListener mListener = new AbstractDeviceListener() {
+        // onConnect() is called whenever a Myo has been connected.
+        @Override
+        public void onConnect(Myo myo, long timestamp) {
+            // Set the text color of the text view to cyan when a Myo connects.
+            mTextView.setTextColor(Color.CYAN);
+            myo.unlock(Myo.UnlockType.HOLD);
+        }
+        // onDisconnect() is called whenever a Myo has been disconnected.
+        @Override
+        public void onDisconnect(Myo myo, long timestamp) {
+            // Set the text color of the text view to red when a Myo disconnects.
+            mTextView.setTextColor(Color.RED);
+        }
+        // onArmSync() is called whenever Myo has recognized a Sync Gesture after someone has put it on their
+        // arm. This lets Myo know which arm it's on and which way it's facing.
+        @Override
+        public void onArmSync(Myo myo, long timestamp, Arm arm, XDirection xDirection) {
+            mTextView.setText(myo.getArm() == Arm.LEFT ? R.string.arm_left : R.string.arm_right);
+        }
+        // onArmUnsync() is called whenever Myo has detected that it was moved from a stable position on a person's arm after
+        // it recognized the arm. Typically this happens when someone takes Myo off of their arm, but it can also happen
+        // when Myo is moved around on the arm.
+        @Override
+        public void onArmUnsync(Myo myo, long timestamp) {
+            mTextView.setText(R.string.hello_world);
+        }
+        // onUnlock() is called whenever a synced Myo has been unlocked. Under the standard locking
+        // policy, that means poses will now be delivered to the listener.
+        @Override
+        public void onUnlock(Myo myo, long timestamp) {
+
+        }
+        // onLock() is called whenever a synced Myo has been locked. Under the standard locking
+        // policy, that means poses will no longer be delivered to the listener.
+        @Override
+        public void onLock(Myo myo, long timestamp) {
+
+        }
+        // onOrientationData() is called whenever a Myo provides its current orientation,
+        // represented as a quaternion.
+        @Override
+        public void onOrientationData(Myo myo, long timestamp, Quaternion rotation) {
+            // Calculate Euler angles (roll, pitch, and yaw) from the quaternion.
+            float roll = (float) Math.toDegrees(Quaternion.roll(rotation));
+            float pitch = (float) Math.toDegrees(Quaternion.pitch(rotation));
+            float yaw = (float) Math.toDegrees(Quaternion.yaw(rotation));
+            // Adjust roll and pitch for the orientation of the Myo on the arm.
+            if (myo.getXDirection() == XDirection.TOWARD_ELBOW) {
+                roll *= -1;
+                pitch *= -1;
+            }
+            // Next, we apply a rotation to the text view using the roll, pitch, and yaw.
+            mTextView.setRotation(roll);
+            mTextView.setRotationX(pitch);
+            mTextView.setRotationY(yaw);
+        }
+        // onPose() is called whenever a Myo provides a new pose.
+        @Override
+        public void onPose(Myo myo, long timestamp, Pose pose) {
+            // Handle the cases of the Pose enumeration, and change the text of the text view
+            // based on the pose we receive.
+            switch (pose) {
+                case UNKNOWN:
+                    mTextView.setText(getString(R.string.hello_world));
+                    break;
+                case REST:
+                case DOUBLE_TAP:
+                    int restTextId = R.string.hello_world;
+                    switch (myo.getArm()) {
+                        case LEFT:
+                            restTextId = R.string.arm_left;
+                            break;
+                        case RIGHT:
+                            restTextId = R.string.arm_right;
+                            break;
+                    }
+                    mTextView.setText(getString(restTextId));
+                    break;
+                case FIST:
+                    mTextView.setText(getString(R.string.pose_fist));
+                    break;
+                case WAVE_IN:
+                    mTextView.setText(getString(R.string.pose_wavein));
+                    break;
+                case WAVE_OUT:
+                    mTextView.setText(getString(R.string.pose_waveout));
+                    break;
+                case FINGERS_SPREAD:
+                    mTextView.setText(getString(R.string.pose_fingersspread));
+                    break;
+            }
+
+            // Stay permanently unlocked
+            myo.unlock(Myo.UnlockType.HOLD);
+        }
+
+        @Override
+        public void onAccelerometerData(Myo myo, long timestamp, Vector3 accel)
+        {
+            accelXData.setText("X: " + Double.toString(accel.x()));
+            accelYData.setText("Y: " + Double.toString(accel.y()));
+            accelZData.setText("Z: " + Double.toString(accel.z()));
+            float[] accelData = {(float)accel.x(), (float)accel.y(), (float)accel.z()};
+            graph.addPoint(accelData);
+        }
+    };
+    // ----------------------------------------------------------------------------------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +176,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Initialize UI variables
+        FrameLayout mainlayout = (FrameLayout)findViewById(R.id.layoutGraph);
+
         Spinner spinner = (Spinner) findViewById(R.id.spinnerInstruments);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -55,7 +188,17 @@ public class MainActivity extends AppCompatActivity {
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
+        mTextView = (TextView) findViewById(R.id.textViewMessage);
+        accelXData = (TextView) findViewById(R.id.textViewAccelX);
+        accelYData = (TextView) findViewById(R.id.textViewAccelY);
+        accelZData = (TextView) findViewById(R.id.textViewAccelZ);
+        graph = new LineGraphView(getApplicationContext(),
+                400,
+                Arrays.asList("X", "Y", "Z"));
+        mainlayout.addView(graph);
+        graph.setVisibility(View.VISIBLE);
         AcquirePerimissions();
+
         // Initialize Myo
         hub = Hub.getInstance();
         if (!hub.init(this)) {
@@ -63,6 +206,9 @@ public class MainActivity extends AppCompatActivity {
             finish();
             return;
         }
+        Hub.getInstance().setLockingPolicy(Hub.LockingPolicy.NONE);
+        Hub.getInstance().setSendUsageData(false);
+
         // Next, register for DeviceListener callbacks.
         hub.addListener(mListener);
     }
